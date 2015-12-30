6f67d025e4288d61f7a746d7fdeb23ce1a7e4164
Adam Long <macadam.long@gmail.com>
2015-12-30 01:20:15 +0000

    inital commit

---
+++ Browser.java
@@ -0,0 +1,39 @@
+import javafx.scene.web.WebView;
+import javafx.embed.swing.JFXPanel;
+import javafx.scene.*;
+import javafx.application.Platform;
+import javax.swing.*;
+import javax.swing.text.*;
+import java.awt.event.*;
+import javafx.concurrent.Worker.State;
+import javafx.beans.value.*;
+import javafx.concurrent.*;
+public class Browser{
+	public static void main(String[] args){
+		JFrame jFrame=new JFrame("Browser");
+
+		JFXPanel jfxPanel=new JFXPanel();
+
+		jFrame.add(jfxPanel);
+
+		jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+		Platform.runLater(() -> {
+			WebView webView=new WebView();
+			jfxPanel.setScene(new Scene(webView));
+			webView.getEngine().load("http://www.ddg.gg/");
+			webView.getEngine().getLoadWorker().stateProperty().addListener(new ChangeListener<State>() {
+				@Override
+				public void changed(ObservableValue ov, State oldState, State newState) {
+
+					if (newState == Worker.State.SUCCEEDED) {
+						System.out.println(webView.getEngine().getLocation());
+					}
+
+				}
+			});
+		});
+
+		jFrame.setVisible(true);
+	}
+}
---
+++ History.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+public class History{
+    ArrayList<String> items;
+    public History(){
+        items=new ArrayList<String>();
+    }
+    public void addItem(String item){
+
+    }
+    public ArrayList<String> getItems(){
+        return items;
+    }
+}
